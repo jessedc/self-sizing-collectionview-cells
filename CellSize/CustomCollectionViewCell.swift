3e959600ee3d8e7022ec08586df8ea5724d4876d
Karl Urdevics <karl@kvurd.com>
2018-08-15 14:16:54 +1000

    fixed rotation issue

--- CellSize/CustomCollectionViewCell.swift
+++ CellSize/CustomCollectionViewCell.swift
@@ -13,31 +13,35 @@ class CustomCollectionViewCell: UICollectionViewCell {
     var title: String? {
         didSet {
             if let title = title {
-                self.configure(withTitle: title)
+                configure(withTitle: title)
             }
         }
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        installConstraints()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        installConstraints()
+    }
 
-    var hasUpdatedConstraints = false
+    func installConstraints() {
+        contentView.addSubview(self.titleLabel)
 
-    override func updateConstraints() {
-        super.updateConstraints()
+        titleLabel.widthAnchor.constraint(equalToConstant: 220.0).isActive = true
+        
+        // if you want a minimum cell height
+        //titleLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 220.0).isActive = true
 
-        if !self.hasUpdatedConstraints {
-            self.contentView.addSubview(self.titleLabel)
-
-            self.hasUpdatedConstraints = true
-
-            self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
-
-            self.titleLabel.widthAnchor.constraint(equalToConstant: 220).isActive = true
-            self.titleLabel.heightAnchor.constraint(equalToConstant: 220).isActive = false
-
-            self.titleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
-            self.titleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
-            self.titleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
-            self.titleLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
-        }
+        titleLabel.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
     }
 
     private lazy var titleLabel: UILabel = {
@@ -54,8 +58,8 @@ class CustomCollectionViewCell: UICollectionViewCell {
     }()
 
     private func configure(withTitle title: String) {
-        self.titleLabel.text = title
+        titleLabel.text = title
 
-        self.setNeedsUpdateConstraints()
+        setNeedsLayout()
     }
 }
